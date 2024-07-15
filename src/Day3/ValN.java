8eb0c75e3619ab80d975cd56ff2ce989c8778a3c
9aram <dlrkxks1028@naver.com>
2024-07-15 22:08:48 +0900

    day2 study

---
+++ src/Day3/ValN.java
@@ -0,0 +1,15 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class ValN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i=0; i<5; i++){
+            int n = sc.nextInt();
+            sum+=n*n;
+        }
+        System.out.println(sum%10);
+    }
+}
